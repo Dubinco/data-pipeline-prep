1f2441327064ee09206570e556d521fee4f347d2
JulesEnguehard <64602187+Dubinco@users.noreply.github.com>
2020-06-22 14:26:28 +0200

    add files via upload

---
+++ nett_IMDB.py
@@ -0,0 +1,23 @@
+from pyspark.sql.functions import lit, when, col
+import pyspark.sql.functions as f
+from pyspark.context import SparkContext
+from pyspark.sql.session import SparkSession
+
+sc = SparkContext('local')
+spark = SparkSession(sc)
+
+df_1980 = spark.read.option("header", "true").option("delimiter", ",").csv(
+    "movie_ratings_1980_2000_p10.csv")
+df_2001 = spark.read.option("header", "true").option("delimiter", ",").csv(
+    "movie_ratings_2010_2001_p25.csv")
+df_2011 = spark.read.option("header", "true").option("delimiter", ",").csv(
+    "movie_ratings_2015_2011_p12.csv")
+#ATTENTION le delimiter est différent ici
+df_2016 = spark.read.option("header", "true").option("delimiter", ";").csv(
+    "movie_ratings_2020_2016_p25.csv")
+
+concat = df_2016.union(df_2011)
+concat = concat.union(df_2001)
+concat = concat.union(df_1980)
+#concat.dropDuplicates()
+concat.show()
